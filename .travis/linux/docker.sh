4568567d903fc13fd8c88b8e74addb1d75c61159
zhupengfei <zhupf321@gmail.com>
2020-04-22 22:34:26 +0800

    Fix docker script
    Did I forget to save this lol

--- .travis/linux/docker.sh
+++ .travis/linux/docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 
-cd /citra
+cd /threeSD
 
 mkdir build && cd build
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ -DENABLE_QT_TRANSLATION=ON -DCITRA_ENABLE_COMPATIBILITY_REPORTING=${ENABLE_COMPATIBILITY_REPORTING:-"OFF"} -DENABLE_COMPATIBILITY_LIST_DOWNLOAD=ON -DUSE_DISCORD_PRESENCE=ON -DENABLE_FFMPEG_VIDEO_DUMPER=ON
+cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++
 ninja
 
 ctest -VV -C Release
