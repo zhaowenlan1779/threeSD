17ea02a1b89f43f3cb7e9ac7d197fc60a80c5f95
Pengfei Zhu <zhupf321@gmail.com>
2022-07-01 18:31:29 +0800

    Update bundled Qt

--- CMakeModules/DownloadExternals.cmake
+++ CMakeModules/DownloadExternals.cmake
@@ -8,7 +8,7 @@ set(prefix "${CMAKE_BINARY_DIR}/externals/${lib_name}")
 if (NOT EXISTS "${prefix}")
     message(STATUS "Downloading binaries for ${lib_name}...")
     file(DOWNLOAD
-        https://github.com/citra-emu/ext-windows-bin/raw/master/${remote_path}${lib_name}.7z
+        https://github.com/yuzu-emu/ext-windows-bin/raw/master/${remote_path}${lib_name}.7z
         "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z" SHOW_PROGRESS)
     execute_process(COMMAND ${CMAKE_COMMAND} -E tar xf "${CMAKE_BINARY_DIR}/externals/${lib_name}.7z"
         WORKING_DIRECTORY "${CMAKE_BINARY_DIR}/externals")
